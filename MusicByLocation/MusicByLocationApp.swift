10c319d17240986c8c898fadc46283da5054cefa
Henry-Hart-School <hart.h@etoncollege.org.uk>
2023-03-02 17:39:18 +0000

    Initial Commit

---
+++ MusicByLocation/MusicByLocationApp.swift
@@ -0,0 +1,17 @@
+//
+//  MusicByLocationApp.swift
+//  MusicByLocation
+//
+//  Created by Hart, Henry (AGDF) on 02/03/2023.
+//
+
+import SwiftUI
+
+@main
+struct MusicByLocationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
